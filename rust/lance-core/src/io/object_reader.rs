c1b2ecd07e0797bf39987b73488f1389c6c7c95a
Weston Pace <weston.pace@gmail.com>
2024-01-14 05:30:51 -0800

    WIP

--- rust/lance-core/src/io/object_reader.rs
+++ rust/lance-core/src/io/object_reader.rs
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::ops::Range;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use std::{ops::Range, sync::atomic::AtomicU64};
 
 use async_trait::async_trait;
 
@@ -47,6 +48,8 @@ impl CloudObjectReader {
     }
 }
 
+static IO_COUNTER: AtomicU64 = AtomicU64::new(0);
+
 #[async_trait]
 impl Reader for CloudObjectReader {
     fn path(&self) -> &Path {
@@ -68,12 +71,34 @@ impl Reader for CloudObjectReader {
         // of the response body. Thus we add an outer retry loop here.
         let mut retries = 3;
         loop {
+            let incremented = IO_COUNTER.fetch_add(1, Ordering::SeqCst);
+            println!(
+                "ADD: There are currently {} I/O operations in flight",
+                incremented + 1
+            );
             match self.object_store.get_range(&self.path, range.clone()).await {
-                Ok(bytes) => return Ok(bytes),
+                Ok(bytes) => {
+                    let decremented = IO_COUNTER.fetch_sub(1, Ordering::SeqCst);
+                    println!(
+                        "OK: There are currently {} I/O operations in flight",
+                        decremented - 1
+                    );
+                    return Ok(bytes);
+                }
                 Err(err) => {
                     if retries == 0 {
+                        let decremented = IO_COUNTER.fetch_sub(1, Ordering::SeqCst);
+                        println!(
+                            "ERR: There are currently {} I/O operations in flight",
+                            decremented - 1
+                        );
                         return Err(err.into());
                     }
+                    let decremented = IO_COUNTER.fetch_sub(1, Ordering::SeqCst);
+                    println!(
+                        "RETRY: There are currently {} I/O operations in flight",
+                        decremented - 1
+                    );
                     retries -= 1;
                 }
             }
