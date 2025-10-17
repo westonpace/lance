d0889cea9383fe5c63210f5b5e4002c6edbba4a0
Weston Pace <weston.pace@gmail.com>
2025-10-17 16:00:55 -0700

    Add test case to reproduce

--- rust/lance-encoding/src/encodings/physical/value.rs
+++ rust/lance-encoding/src/encodings/physical/value.rs
@@ -1167,6 +1167,16 @@ pub(crate) mod tests {
     }
 
     #[test_log::test(tokio::test)]
+    async fn test_mixed_page_validity() {
+        let no_nulls = Arc::new(Int32Array::from_iter_values([1, 2]));
+        let has_nulls = Arc::new(Int32Array::from_iter([Some(3), None, Some(5)]));
+
+        let test_cases = TestCases::default().with_page_sizes(vec![1]);
+        check_round_trip_encoding_of_data(vec![no_nulls, has_nulls], &test_cases, HashMap::new())
+            .await;
+    }
+
+    #[test_log::test(tokio::test)]
     async fn test_value_encoding_verification() {
         use std::collections::HashMap;
 
